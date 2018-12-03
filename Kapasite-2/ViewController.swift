bc543f2c97ea6df45778c5fd583928ceb4d438d6
BSH_MRM <bsh_mrm@bsh-mrm-macbook-pro.local>
2018-12-03 13:51:27 +0300

    Initial Commit

---
+++ Kapasite-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kapasite-2
+//
+//  Created by BSH_MRM on 3.12.2018.
+//  Copyright © 2018 BSH_MRM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
